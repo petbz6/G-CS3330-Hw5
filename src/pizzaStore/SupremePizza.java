251c34ebf1ffd9471a687ef48f2664711fd44f7e
Isaac Rider <111994726+IsaacRider@users.noreply.github.com>
2024-03-01 08:55:05 -0600

    Update SupremePizza.java

--- src/pizzaStore/SupremePizza.java
+++ src/pizzaStore/SupremePizza.java
@@ -1,5 +1,44 @@
 package pizzaStore;
 
-public class SupremePizza {
+public class SupremePizza extends AbstractPizza{
 
+	public SupremePizza() {
+        super();
+        this.priceWithoutToppings = 3.50;
+        this.toppingList.add(Toppings.TOMATO);
+        this.toppingList.add(Toppings.CHEESE);
+        this.toppingList.add(Toppings.BELL_PEPPER);
+        this.toppingList.add(Toppings.ITALIAN_SAUSAGE);
+        this.toppingList.add(Toppings.PEPPERONI);
+        this.toppingList.add(Toppings.BLACK_OLIVE);
+        this.toppingList.add(Toppings.MUSHROOM);
+        this.totalPrice = addToppingsToPrice(this.priceWithoutToppings);
+    }
+	
+	public SupremePizza(AbstractPizza abstractPizza) {
+		super(abstractPizza);
+	}
+	
+	@Override
+	protected double addToppingsToPrice(double priceWithoutToppings) {
+		double totalPrice = priceWithoutToppings;
+        for (Toppings topping : toppingList) {
+        	totalPrice += topping.getToppingPrice();
+        }
+		return totalPrice;
+	}
+
+	@Override
+	public double updatePizzaPrice() {
+		this.totalPrice = addToppingsToPrice(this.priceWithoutToppings);
+        return this.totalPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "SupremePizza [toppingList=" + toppingList + ", priceWithoutToppings=" + priceWithoutToppings
+				+ ", totalPrice=" + totalPrice + ", abstractPizzaOrderID=" + abstractPizzaOrderID + ", cookingStrategy="
+				+ cookingStrategy + ", cookingPrice=" + cookingPrice + ", updatePizzaPrice()=" + updatePizzaPrice()
+				+ "]";
+	}
 }
